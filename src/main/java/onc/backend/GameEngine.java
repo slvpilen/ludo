df16a9cf6d3902933c628ee9f5b13f080281b8e9
Christian Fredrik <cjohnsen41@gmail.com>
2023-03-28 00:27:34 +0200

    endret en liten robot-feil

--- src/main/java/onc/backend/GameEngine.java
+++ src/main/java/onc/backend/GameEngine.java
@@ -202,8 +202,8 @@ public class GameEngine {
         fireRobotRolledDice();
         
         while (robot.equals(currentPlayer)) {
-            fireRobotRolledDice();
             robot.makeRobotMove();
+            fireRobotRolledDice();
         }
     }
 
