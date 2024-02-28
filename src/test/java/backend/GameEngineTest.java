8da6859bf94d2ba25177fbf4ace534adca3749ed
Christian Fredrik <cjohnsen41@gmail.com>
2024-02-28 21:03:52 +0100

    Remove unused imports in GameEngineTest.java

--- src/test/java/backend/GameEngineTest.java
+++ src/test/java/backend/GameEngineTest.java
@@ -8,7 +8,6 @@ import onc.backend.Settings;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -19,7 +18,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javafx.print.Collation;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
